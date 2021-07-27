c689a0d696b1fa7345b16b170da44c184ab7c561
supawich_r <supawich_r@elearning.cmu.ac.th>
2021-07-27 23:04:47 +0700

    5.2

---
+++ lab-vue/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    props: (route) => ({
+      page: parseInt(route.query.page) || 0,
+      perPage: parseInt(route.query.perPage) || 10
+    })
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
